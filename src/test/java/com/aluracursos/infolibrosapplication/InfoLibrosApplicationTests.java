6bd53413849cd9cc0d935f4c78af0600174890b1
juangarafulic <jgarafulic@hotmail.com>
2024-05-18 12:58:41 -0400

    primer commit

---
+++ src/test/java/com/aluracursos/infolibrosapplication/InfoLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.infolibrosapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
