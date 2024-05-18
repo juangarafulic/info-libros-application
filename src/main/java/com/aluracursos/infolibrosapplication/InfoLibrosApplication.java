1eafbcfc174fc5ef11d1958ad7bb191ace99eb77
juangarafulic <jgarafulic@hotmail.com>
2024-05-18 14:13:32 -0400

    crea modelos tipo record para mapear datos json

--- src/main/java/com/aluracursos/infolibrosapplication/InfoLibrosApplication.java
+++ src/main/java/com/aluracursos/infolibrosapplication/InfoLibrosApplication.java
@@ -1,13 +1,20 @@
 package com.aluracursos.infolibrosapplication;
 
+import com.aluracursos.infolibrosapplication.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfoLibrosApplication {
+public class InfoLibrosApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(InfoLibrosApplication.class, args);
     }
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.muestraMenu();
+	}
 }
--- src/main/java/com/aluracursos/infolibrosapplication/principal/Principal.java
+++ src/main/java/com/aluracursos/infolibrosapplication/principal/Principal.java
@@ -1,7 +1,15 @@
 package com.aluracursos.infolibrosapplication.principal;
 
-public class Principal {
-    public void muestraMenu() {
+import com.aluracursos.infolibrosapplication.service.ConsumoAPI;
+import com.aluracursos.infolibrosapplication.service.ConvierteDatos;
 
+public class Principal {
+    private static final String URL_BASE = "https://gutendex.com/books/";
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu() {
+        var json = consumoAPI.obtenerDatos(URL_BASE);
+        System.out.println(json);
     }
 }
