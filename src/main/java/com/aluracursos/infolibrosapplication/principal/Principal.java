70ce6b869e7ea130da2c6944377017238e3ffc1f
juangarafulic <jgarafulic@hotmail.com>
2024-05-18 14:37:20 -0400

    funcion de busqueda de top 10 mas descargados

--- src/main/java/com/aluracursos/infolibrosapplication/principal/Principal.java
+++ src/main/java/com/aluracursos/infolibrosapplication/principal/Principal.java
@@ -1,8 +1,12 @@
 package com.aluracursos.infolibrosapplication.principal;
 
 import com.aluracursos.infolibrosapplication.model.Datos;
+import com.aluracursos.infolibrosapplication.model.DatosLibros;
 import com.aluracursos.infolibrosapplication.service.ConsumoAPI;
 import com.aluracursos.infolibrosapplication.service.ConvierteDatos;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -12,7 +16,18 @@ public class Principal {
     public void muestraMenu() {
         var json = consumoAPI.obtenerDatos(URL_BASE);
         System.out.println(json);
+
+        //Conviertiendo datos json a datos Java
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        //Top 10 libros más descargados
+        System.out.println("Top 10 libros más descargados");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibros::numeroDescargas).reversed())
+                .limit(10)
+                .map(libro -> libro.titulo().toUpperCase())
+                .forEach(System.out::println);
+
     }
 }
